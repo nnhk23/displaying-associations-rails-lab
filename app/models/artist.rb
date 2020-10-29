13eb42de95250d7705ba221c08f74288c78bbd26
Kim Nguyen <nnhk23@gmail.com>
2020-10-29 15:27:36 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,14 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        count = 0
+        Artist.all.each do |artist|
+            Song.all.each do |song|
+                song.artist_id == artist.id ? count += 1 : count += 0
+            end
+        return count
+        end
+    end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    Song.all.collect do |song|
+      song.artist.name
+    end.first
+  end
+
+
 end
